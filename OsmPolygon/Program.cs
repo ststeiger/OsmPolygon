a47043df319e7e451b7323f0c1f6b964fb670906
ststeiger <quandary82@hailmail.net>
2020-01-13 13:26:29 +0100

    ESRI-Fix

--- OsmPolygon/Program.cs
+++ OsmPolygon/Program.cs
@@ -9,31 +9,37 @@ namespace OsmPolygon
 
         static void Main(string[] args)
         {
+            // EsriConverter.ESRI.Test();
+            // MoveMe.MoveMe.TestArea();
+
+
             // args = new string[] { "464651233", "95691336", "148117240", "104041936", "43012904", "49589463", "224285187", "58080194", "479999588", "218557958"  };
-            // args = new string[] { "224267897", "224269589" };
-            // args = new string[] { "58080208", "464651232" };
-            // args = new string[] { "58080208", "464651232" };
-            // args = new string[] { "690355074", "690355077", "690355073" };
-            // args = new string[] { "690355074" };
-            //args = new string[] { "326116406", "176675521" };
+            // args = new string[] { "224267897", "224269589" }; 
+            // args = new string[] { "58080208", "464651232" };  
+            // args = new string[] { "58080208", "464651232" }; 
+            // args = new string[] { "690355074", "690355077", "690355073" }; 
+            // args = new string[] { "690355074" }; 
+            // args = new string[] { "326116406", "176675521" }; 
 
             // Do it all automatically: 
-            // OsmPolyonFinder.GetAndInsertBuildingPolygon();
+            // OsmPolyonFinder.GetAndInsertBuildingPolygon(); 
+            args = new string[] { "100787726", "100787718", "337954728"}; 
+
 
             for (int i = 0; i < args.Length; ++i)
-            {
-                string way = args[i];
-                System.Console.WriteLine($"i[{i}]: {way}");
+            { 
+                string way = args[i]; 
+                System.Console.WriteLine($"i[{i}]: {way}"); 
 
                 string script = OSM.API.v0_6.Polygon.GetPointsInsert(way, "AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA"); 
-                System.IO.File.WriteAllText(way + ".sql", script, System.Text.Encoding.UTF8);
-                System.Console.WriteLine(script);
+                System.IO.File.WriteAllText(way + ".sql", script, System.Text.Encoding.UTF8); 
+                System.Console.WriteLine(script); 
             } // Next i 
 
-            System.Console.WriteLine(" --- Press any key to continue --- ");
-            while (!System.Console.KeyAvailable)
-            {
-                System.Threading.Thread.Sleep(100);
+            System.Console.WriteLine(" --- Press any key to continue --- "); 
+            while (!System.Console.KeyAvailable) 
+            { 
+                System.Threading.Thread.Sleep(100); 
             } // Whend 
 
         } // End Sub Main 
