c957c57e29aade3a1a269b1816fa183b77612b2b
ststeiger <quandary82@hailmail.net>
2020-07-24 15:53:10 +0200

    Fix Style

--- OsmPolygon/Code/OsmPolygon/OldOsmPolygonHelper.cs
+++ OsmPolygon/Code/OsmPolygon/OldOsmPolygonHelper.cs
@@ -3,7 +3,6 @@ namespace OsmPolygon
 {
 
 
-
     // A C# program to check if a given point 
     // lies inside a given polygon 
     // Refer https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/ 
@@ -17,10 +16,11 @@ namespace OsmPolygon
         // caused overflow problems) 
         private static int INF = 10000;
 
+
         // Given three colinear points p, q, r,  
         // the function checks if point q lies 
         // on line segment 'pr' 
-        private static bool onSegment(GeoApis.LatLng p, GeoApis.LatLng q, GeoApis.LatLng r)
+        private static bool OnSegment(GeoApis.LatLng p, GeoApis.LatLng q, GeoApis.LatLng r)
         {
             if (q.lng <= System.Math.Max(p.lng, r.lng) &&
                 q.lng >= System.Math.Min(p.lng, r.lng) &&
@@ -30,14 +30,15 @@ namespace OsmPolygon
                 return true;
             }
             return false;
-        }
+        } // End Function OnSegment 
+
 
         // To find orientation of ordered triplet (p, q, r). 
         // The function returns following values 
         // 0 --> p, q and r are colinear 
         // 1 --> Clockwise 
         // 2 --> Counterclockwise 
-        private static int orientation(GeoApis.LatLng p, GeoApis.LatLng q, GeoApis.LatLng r)
+        private static int Orientation(GeoApis.LatLng p, GeoApis.LatLng q, GeoApis.LatLng r)
         {
             decimal val = (q.lat - p.lat) * (r.lng - q.lng) -
                       (q.lng - p.lng) * (r.lat - q.lat);
@@ -47,19 +48,20 @@ namespace OsmPolygon
                 return 0; // colinear 
             }
             return (val > 0) ? 1 : 2; // clock or counterclock wise 
-        }
+        } // End Function Orientation 
+
 
         // The function that returns true if  
         // line segment 'p1q1' and 'p2q2' intersect. 
-        private static bool doIntersect(GeoApis.LatLng p1, GeoApis.LatLng q1,
+        private static bool DoIntersect(GeoApis.LatLng p1, GeoApis.LatLng q1,
                                 GeoApis.LatLng p2, GeoApis.LatLng q2)
         {
             // Find the four orientations needed for  
             // general and special cases 
-            int o1 = orientation(p1, q1, p2);
-            int o2 = orientation(p1, q1, q2);
-            int o3 = orientation(p2, q2, p1);
-            int o4 = orientation(p2, q2, q1);
+            int o1 = Orientation(p1, q1, p2);
+            int o2 = Orientation(p1, q1, q2);
+            int o3 = Orientation(p2, q2, p1);
+            int o4 = Orientation(p2, q2, q1);
 
             // General case 
             if (o1 != o2 && o3 != o4)
@@ -70,35 +72,35 @@ namespace OsmPolygon
             // Special Cases 
             // p1, q1 and p2 are colinear and 
             // p2 lies on segment p1q1 
-            if (o1 == 0 && onSegment(p1, p2, q1))
+            if (o1 == 0 && OnSegment(p1, p2, q1))
             {
                 return true;
             }
 
             // p1, q1 and p2 are colinear and 
             // q2 lies on segment p1q1 
-            if (o2 == 0 && onSegment(p1, q2, q1))
+            if (o2 == 0 && OnSegment(p1, q2, q1))
             {
                 return true;
             }
 
             // p2, q2 and p1 are colinear and 
             // p1 lies on segment p2q2 
-            if (o3 == 0 && onSegment(p2, p1, q2))
+            if (o3 == 0 && OnSegment(p2, p1, q2))
             {
                 return true;
             }
 
             // p2, q2 and q1 are colinear and 
             // q1 lies on segment p2q2 
-            if (o4 == 0 && onSegment(p2, q1, q2))
+            if (o4 == 0 && OnSegment(p2, q1, q2))
             {
                 return true;
             }
 
             // Doesn't fall in any of the above cases 
             return false;
-        }
+        } // End Function DoIntersect 
 
 
 
@@ -128,15 +130,15 @@ namespace OsmPolygon
                 // Check if the line segment from 'p' to  
                 // 'extreme' intersects with the line  
                 // segment from 'polygon[i]' to 'polygon[next]' 
-                if (doIntersect(polygon[i],
+                if (DoIntersect(polygon[i],
                                 polygon[next], p, extreme))
                 {
                     // If the point 'p' is colinear with line  
                     // segment 'i-next', then check if it lies  
                     // on segment. If it lies, return true, otherwise false 
-                    if (orientation(polygon[i], p, polygon[next]) == 0)
+                    if (Orientation(polygon[i], p, polygon[next]) == 0)
                     {
-                        return onSegment(polygon[i], p,
+                        return OnSegment(polygon[i], p,
                                          polygon[next]);
                     }
                     count++;
@@ -146,10 +148,9 @@ namespace OsmPolygon
 
             // Return true if count is odd, false otherwise 
             return (count % 2 == 1); // Same as (count%2 == 1) 
-        }
+        } // End Function IsInside 
 
 
-        // Driver Code 
         public static void Test()
         {
             //GeoApis.LatLng[] polygon1 = {new GeoApis.LatLng(0, 0),
@@ -179,10 +180,10 @@ namespace OsmPolygon
 
             bool inside = IsInside(polygon1, p);
             System.Console.WriteLine(inside);
-        }
+        } // End Sub Test 
 
 
-    } // End Class 
+    } // End Class OldOsmPolygonHelper 
 
 
 } // End Namespace 
--- OsmPolygon/Code/OsmPolygon/OsmPolygonHelper.cs
+++ OsmPolygon/Code/OsmPolygon/OsmPolygonHelper.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace OsmPolygon
 {
+
     using OpenToolkit.Mathematics;
 
 
@@ -17,10 +18,11 @@ namespace OsmPolygon
         // caused overflow problems) 
         static int INF = 10000;
 
+
         // Given three colinear points p, q, r,  
         // the function checks if point q lies 
         // on line segment 'pr' 
-        private static bool onSegment(DecimalVector2 p, DecimalVector2 q, DecimalVector2 r)
+        private static bool OnSegment(DecimalVector2 p, DecimalVector2 q, DecimalVector2 r)
         {
             if (q.X <= System.Math.Max(p.X, r.X) &&
                 q.X >= System.Math.Min(p.X, r.X) &&
@@ -30,36 +32,38 @@ namespace OsmPolygon
                 return true;
             }
             return false;
-        }
+        } // End Function OnSegment 
+
 
         // To find orientation of ordered triplet (p, q, r). 
         // The function returns following values 
         // 0 --> p, q and r are colinear 
         // 1 --> Clockwise 
         // 2 --> Counterclockwise 
-        private static int orientation(DecimalVector2 p, DecimalVector2 q, DecimalVector2 r)
+        private static int Orientation(DecimalVector2 p, DecimalVector2 q, DecimalVector2 r)
         {
-            decimal val = (q.Y - p.Y) * (r.X - q.X) -
-                      (q.X - p.X) * (r.Y - q.Y);
+            decimal val = (q.Y - p.Y) * (r.X - q.X) - (q.X - p.X) * (r.Y - q.Y);
 
             if (val == 0)
             {
                 return 0; // colinear 
             }
+
             return (val > 0) ? 1 : 2; // clock or counterclock wise 
-        }
+        } // End Function Orientation 
+
 
         // The function that returns true if  
         // line segment 'p1q1' and 'p2q2' intersect. 
-        private static bool doIntersect(DecimalVector2 p1, DecimalVector2 q1,
+        private static bool DoIntersect(DecimalVector2 p1, DecimalVector2 q1,
                                 DecimalVector2 p2, DecimalVector2 q2)
         {
             // Find the four orientations needed for  
             // general and special cases 
-            int o1 = orientation(p1, q1, p2);
-            int o2 = orientation(p1, q1, q2);
-            int o3 = orientation(p2, q2, p1);
-            int o4 = orientation(p2, q2, q1);
+            int o1 = Orientation(p1, q1, p2);
+            int o2 = Orientation(p1, q1, q2);
+            int o3 = Orientation(p2, q2, p1);
+            int o4 = Orientation(p2, q2, q1);
 
             // General case 
             if (o1 != o2 && o3 != o4)
@@ -70,35 +74,35 @@ namespace OsmPolygon
             // Special Cases 
             // p1, q1 and p2 are colinear and 
             // p2 lies on segment p1q1 
-            if (o1 == 0 && onSegment(p1, p2, q1))
+            if (o1 == 0 && OnSegment(p1, p2, q1))
             {
                 return true;
             }
 
             // p1, q1 and p2 are colinear and 
             // q2 lies on segment p1q1 
-            if (o2 == 0 && onSegment(p1, q2, q1))
+            if (o2 == 0 && OnSegment(p1, q2, q1))
             {
                 return true;
             }
 
             // p2, q2 and p1 are colinear and 
             // p1 lies on segment p2q2 
-            if (o3 == 0 && onSegment(p2, p1, q2))
+            if (o3 == 0 && OnSegment(p2, p1, q2))
             {
                 return true;
             }
 
             // p2, q2 and q1 are colinear and 
             // q1 lies on segment p2q2 
-            if (o4 == 0 && onSegment(p2, q1, q2))
+            if (o4 == 0 && OnSegment(p2, q1, q2))
             {
                 return true;
             }
 
             // Doesn't fall in any of the above cases 
             return false;
-        }
+        } // End Function DoIntersect 
 
 
         // Returns true if the point p lies  
@@ -106,7 +110,6 @@ namespace OsmPolygon
         public static bool IsInside(System.Collections.Generic.IList<DecimalVector2> polygon,  DecimalVector2 p)
         {
             int n = polygon.Count;
-
 
             // There must be at least 3 vertices in polygon[] 
             if (n < 3)
@@ -127,25 +130,25 @@ namespace OsmPolygon
                 // Check if the line segment from 'p' to  
                 // 'extreme' intersects with the line  
                 // segment from 'polygon[i]' to 'polygon[next]' 
-                if (doIntersect(polygon[i],
-                                polygon[next], p, extreme))
+                if (DoIntersect(polygon[i], polygon[next], p, extreme))
                 {
                     // If the point 'p' is colinear with line  
                     // segment 'i-next', then check if it lies  
                     // on segment. If it lies, return true, otherwise false 
-                    if (orientation(polygon[i], p, polygon[next]) == 0)
+                    if (Orientation(polygon[i], p, polygon[next]) == 0)
                     {
-                        return onSegment(polygon[i], p,
-                                         polygon[next]);
+                        return OnSegment(polygon[i], p, polygon[next]);
                     }
+
                     count++;
-                }
+                } // End if (DoIntersect(polygon[i], polygon[next], p, extreme)) 
+
                 i = next;
             } while (i != 0);
 
             // Return true if count is odd, false otherwise 
             return (count % 2 == 1); // Same as (count%2 == 1) 
-        }
+        } // End Function IsInside 
 
 
         public static void Test()
@@ -172,10 +175,10 @@ namespace OsmPolygon
 
             bool inside = IsInside(polygon1, p);
             System.Console.WriteLine(inside);
-        }
+        } // End Sub Test 
 
 
-    } // End Class 
+    } // End Class OsmPolygonHelper 
 
 
 } // End Namespace 
